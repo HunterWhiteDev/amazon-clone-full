85e3c42cbcf15532e1797ab705217e40ee1f5153
Hunter <hunterwhitedev@gmail.com>
2021-01-03 13:50:30 -0500

    Setup react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-      {/*  */}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/checkout">
+          <Header />
+          <h1>I AM A CHECKOUT</h1>
+        </Route>
+        <Route path="/">
+          <div className="app">
+            <Header />
+            <Home />
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
