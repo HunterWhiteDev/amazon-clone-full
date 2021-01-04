d8d777f5246edb3944523076a104ac2e76e6b610
Hunter <hunterwhitedev@gmail.com>
2021-01-04 06:55:47 -0500

    Front-end login page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
-      <Header />
       <Switch>
         <Route path="/checkout">
+          <Header />
           <Checkout />
+        </Route>
+        <Route path="/login">
+          <Login />
         </Route>
         <Route path="/">
           <div className="app">
+            <Header />
             <Home />
           </div>
         </Route>
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB3tadpttkR2g_ErwP993E54tRTrts2t5w",
+  authDomain: "clone-full-26b84.firebaseapp.com",
+  projectId: "clone-full-26b84",
+  storageBucket: "clone-full-26b84.appspot.com",
+  messagingSenderId: "803783824905",
+  appId: "1:803783824905:web:1d0f3d4a947bc86a3acad8",
+  measurementId: "G-CM32BS2FX9",
+};
